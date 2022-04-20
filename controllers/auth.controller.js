7614744cdf519aebbb99481e18680fda0ae4f116
drondon87 <domingorondon87@gmail.com>
2022-04-20 18:39:41 +0200

    Loggeo desde google

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const bcryptjs = require('bcryptjs');
 const { response, request } = require('express');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 const Usuario = require('../models/usuario.model');
 
 const login = async(req = request, res = response) => {
@@ -43,11 +44,55 @@ const login = async(req = request, res = response) => {
     } catch (error) {
         console.log(error);
         res.status(500).json({
+            ok: false,
+            msg: 'Error al intentar loggear con google'
+        });
+    }
+}
+
+const googleSignIn = async(req = request, res = response) => {
+
+    const { id_token } = req.body;
+    try {
+
+        const { nombre, img, correo } = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({ correo });
+
+        if (!usuario) {
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true,
+                rol: 'USER_ROLE'
+            }
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        // Si el usuario en BD existe
+        if (!usuario.estado) {
+            return res.status(401).json({
+                msg: 'Hable con el Administrador, usuario Bloqueado'
+            })
+        }
+
+        // Generar el JWT
+        const token = await generarJWT(usuario.id);
+
+        res.json({ usuario, token })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
             msg: 'Error al intentar loggear'
         });
     }
 }
 
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
